6d300007be6c8ce6fb9e89d2ec7fb529aa798bcd
gabjordan <gabcjordan@gmail.com>
2026-04-14 19:35:34 -0700

    /users step

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,6 +3,35 @@ import express from "express";
 
 const app = express();
 const port = 8000;
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
 
 app.use(express.json());
 
@@ -10,8 +39,13 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
 });
+
