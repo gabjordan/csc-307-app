01b860697f733cea6560a8e5d3c738573a33d869
gabjordan <gabcjordan@gmail.com>
2026-05-01 02:17:34 -0700

    name/job table -> list view

--- src/Table.jsx
+++ src/Table.jsx
@@ -5,24 +5,35 @@ function TableHeader() {
   return (
     <thead>
       <tr>
+        <th>Completed</th>
         <th>Name</th>
-        <th>Job</th>
-        <th>ID</th>
+        <th>Date</th>
+        <th>Repeating</th>
       </tr>
     </thead>
   );
 }
 
+/* TO DO:
+- removeCharacter -> removeTask (+ corresponding functions)
+- same w/ edit button*/
+
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
+        <td>{row.completed}</td>
         <td>{row.name}</td>
-        <td>{row.job}</td>
-        <td>{row.id}</td>
+        <td>{row.date}</td>
+        <td>{row.repeating}</td>
         <td>
             <button onClick={() => props.removeCharacter(index)}>
                 Delete
+            </button>
+        </td>
+        <td>
+            <button onClick={() => props.removeCharacter(index)}>
+              Edit
             </button>
         </td>
       </tr>
